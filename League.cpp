a76d0694132e3adb8e3b99377a4956e51b0030af
Assia Benachir <assia.benachir@polytechnique.edu>
2018-12-06 08:19:03 +0100

    league class calendar finished

--- League.cpp
+++ League.cpp
@@ -1,20 +1,35 @@
 #include "League.hpp"
 #include <string>
+#include <list>
 
 //random names for teams to be changed as well
 std::list teamNames = {"Lakers","Cavaliers","Raptors","Warriors","Thunders","76ers","Bulls","Wizards","Celtics","Pacers","Nets","Hawks"};
 
 
-League::League(int div, std::string seas){          //Takes arguments : division as an int and a season as a string
-    division = division;
+League::League(int divi, std::string seas){          //Takes arguments : division as an int and a season as a string
+    division = divi ;
     season = seas;
     for (int i=0, i<12, i++){
         Team t=Team( teamNames[rand()%12] );   
         League::teams.push_back(t);
     }
+
+    /*std::map<std::string, double,std::list> attributes;
+    attributes["season"] = seas;
+    attributes["division"] = divi;
+    */
 }
 
 
 League::calendar() {
-
+    std::map<int,std::list> calendar;
+    for (int i, i=0, i<12, i++){   //iterating over each team
+        for (int w, w=1, w++){   //iterating over all weeks for each team
+        std::string match= teams[i].name +' vs. '+ teams[w%11].name ;
+        calendar[w].push_back(match);
+        }
+    }
+    return calendar;
 }
+
+
--- League.hpp
+++ League.hpp
@@ -14,4 +14,18 @@ class League{   //has 12 teams competing during 22 weeks
     
     //functions
     std::map calendar();
+    
 }
+
+//Possible implementation of a get attribute function, would need to implement a dictionarry to get all the attributes values
+
+/*template < typename T> T get_attribute(std::string attributeName){
+    return attributes[attributeName];  //here attributes would be a dictionnary instead of a list 
+}
+
+template <typename T> void set_attribute(std::string attributeName, T value){
+    attributes[attributeName] = value;  //same comment as before for attributes
+}
+
+*/ 
+
