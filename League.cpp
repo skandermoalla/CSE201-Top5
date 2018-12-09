5261441ce02ce088adfd2c1a0d2f5b57dec31a13
SannyaBTM <45061456+SannyaBTM@users.noreply.github.com>
2018-12-09 14:17:33 +0000

    Updated
    Added calendar function (debugged but needs tests)
    Takes into account Skander's comments
    season is of the format "18/19", for decoration

--- League.cpp
+++ League.cpp
@@ -1,21 +1,20 @@
 #include "League.hpp"
 #include <string>
 #include <list>
-
+#include <cstdlib>
+#include <cstdio>
 //random names for teams to be changed as well
 
-//why a list? ( 'list' is a linked list in cpp)
 
-std::list teamNames = {"Lakers","Cavaliers","Raptors","Warriors","Thunders","76ers","Bulls","Wizards","Celtics","Pacers","Nets","Hawks"};
-
+std::string teamNames[] = { "Lakers","Cavaliers","Raptors","Warriors","Thunders","76ers","Bulls","Wizards","Celtics","Pacers","Nets","Hawks" };
 
 // why a string for seas? how are you going to increment it? it should be an int.
 League::League(int divi, std::string seas){          //Takes arguments : division as an int and a season as a string
     division = divi ;
     season = seas;
-    for (int i=0, i<12, i++){
-        // rand? so two teams can have the same name???
-        Team t=Team( teamNames[rand()%12] );   
+	int n = rand()%12 ; 
+	for (int i = 0; i < 12; i++) {
+        Team t=Team( teamNames[(n + i) % 12] );
         League::teams.push_back(t);
     }
 
@@ -25,17 +24,53 @@ League::League(int divi, std::string seas){          //Takes arguments : divisio
     */
 }
 
+std::map<int, std::list< std::tuple<int,int> > > League::calendar()
+{
+	std::map<int, std::list< std::tuple<int, int> > > calendar;
 
-League::calendar() {
-    //did you mean to use a vector or a list?
-    std::map<int,std::list> calendar;
-    for (int i, i=0, i<12, i++){   //iterating over each team
-        for (int w, w=1, w++){   //iterating over all weeks for each team
-        std::string match= teams[i].name +' vs. '+ teams[w%11].name ;
-        calendar[w].push_back(match);
-        }
-    }
-    return calendar;
+	//creating map assigning its possible opponents to each team
+	std::map<int, std::vector<int> > possibilities;
+	for (int i = 1; i < 13; i++) {
+		std::vector<int> teams = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+		teams.erase(teams.begin() + i - 1);
+		possibilities[i] = teams;
+	}
+
+	//create list of matches for each week
+	for (int w = 1; w < 23; w++) {
+		std::vector<int> freeTeams = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+		while (freeTeams.size() > 0) {
+			int t = freeTeams[0];
+
+			std::vector<int> possible_opponents = intersection(freeTeams, possibilities[t] ); 
+
+			int opponent = possible_opponents[0];  //taking the first possible and match-free opponent
+
+			std::tuple<int, int> match = std::make_tuple(t, opponent);
+
+			calendar[w].push_back(match);  //adding match to the list of matches of the week
+
+			//deleting the teams which already have a match assigned to them
+			freeTeams.erase(freeTeams.begin() + opponent - t); 
+			freeTeams.erase(freeTeams.begin());
+		}
+	}
+
+	return calendar;
+}
+
+
+//auxiliary function to find the intersection of two vectors 
+std::vector<int> intersection(std::vector<int> vect1, std::vector<int> vect2) {
+	std::vector<int> intersect;
+	for (int i = 0; i < vect1.size(); i++) {
+		for (int j = 0; j < vect2.size(); j++) {
+			if (vect1[i] == vect2[j]) {
+				intersect.push_back(vect1[i]);
+			}
+		}
+	}
+	return intersect;
 }
 
 
--- League.hpp
+++ League.hpp
@@ -1,5 +1,7 @@
+#pragma once 
 #include <string>
 #include <map>
+#include <tuple>
 #include "Team.cpp"
 
 class League{   //has 12 teams competing during 22 weeks
@@ -13,19 +15,18 @@ class League{   //has 12 teams competing during 22 weeks
     League(int div, std::string seas);
     
     //functions
-    std::map calendar();
+	std::map<int, std::list< std::tuple<int,int> > > calendar();
     
 }
 
 //Possible implementation of a get attribute function, would need to implement a dictionarry to get all the attributes values
 
 /*template < typename T> T get_attribute(std::string attributeName){
-    return attributes[attributeName];  //here attributes would be a dictionnary instead of a list 
+	return attributes[attributeName];  //here attributes would be a dictionnary instead of a list
 }
 
 template <typename T> void set_attribute(std::string attributeName, T value){
-    attributes[attributeName] = value;  //same comment as before for attributes
+	attributes[attributeName] = value;  //same comment as before for attributes
 }
 
-*/ 
-
+*/;
