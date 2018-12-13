fffd30df7f4d34a49617be57a08607a182890b6e
SannyaBTM <45061456+SannyaBTM@users.noreply.github.com>
2018-12-13 07:25:01 +0000

    Update TestingLeague.cpp

--- TestingLeague.cpp
+++ TestingLeague.cpp
@@ -6,20 +6,10 @@ int main{
 		League L1 = League::League(1,'2018/2019');
 		League L2 = League::League(2, '2019/2020');
 		League L3 = League::League(3, '2020/2021');
-		std::cout << L1.get_division(), L2.get_division(), L3.get_division() << std::endl;
-		std::cout << L1.get_season(), L2.get_season(), L3.get_season() << std::endl;
+		std::cout << L1.division, L2.division, L3.division << std::endl;
+		std::cout << L1.season, L2.season, L3.season << std::endl;
 
-		L1.set_division(3);
-		L2.set_division(1);
-		L3.set_division(2);
 		
-		std::cout << L1.get_division(), L2.get_division(), L3.get_division() << std::endl;
-
-		L1.set_season("2020/2021");
-		L2.set_season("2019/2020");
-		L3.set_season("2018/2019");
-
-		std::cout << L1.get_season(), L2.get_season(), L3.get_season() << std::endl;
-
+		std::cout << L1.teams, L2.teams, L3.teams << std::endl;
 }
 
