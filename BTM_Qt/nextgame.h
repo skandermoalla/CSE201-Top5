9815e22d5c75fbc97a7f862499d7f6f00a434081
kderkba <kaderki@hotmail.fr>
2018-12-20 10:34:57 +0100

    Create nextgame.h
    committing the file with nextgame window. the window is not finished yet.

---
+++ BTM_Qt/nextgame.h
@@ -0,0 +1,27 @@
+#ifndef NEXTGAME_H
+#define NEXTGAME_H
+#include <QLCDNumber>
+#include <QDialog>
+#include<QTimer>
+namespace Ui {
+class NextGame;
+}
+
+class NextGame : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit NextGame(QWidget *parent = nullptr);
+    ~NextGame();
+public slots:
+    void myfunc();//function that set and display the time
+private slots:
+    void on_start_clicked(); //click start function
+
+private:
+    Ui::NextGame *ui;
+    QTimer *timer;
+};
+
+#endif // NEXTGAME_H
