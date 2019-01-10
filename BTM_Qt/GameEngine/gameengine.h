918db268e48494bdc3a416c557cdeaab10b4e4b9
Skander Moalla <skander.moalla@polytechnique.edu>
2019-01-10 07:48:01 +0100

    Editing simulatethisweeksGames

--- BTM_Qt/GameEngine/gameengine.h
+++ BTM_Qt/GameEngine/gameengine.h
@@ -14,21 +14,20 @@ public:
     //methods
 
     //simulates the games that the manager is not playing for the current League's week
-    void simulateThisWeeksGames(League league);
+    void simulateThisWeeksGames(League& league);
 
     //simulates a Game not involving the manager between two teams and update their attributes according to the final score
-    void simulateAutomatedGame(League league, Team team1, Team team2);
+    void simulateAutomatedGame(League& league, Team& team1, Team& team2);
 
     // (loading...)
-    void playThisWeeksGame(League league);
+    void playThisWeeksGame(League& league, Team& opponentsTeam);
 
 private:
     //returns the outcome of a automatic game (scoreOfTeam1, scoreOfTeam2)
-    std::pair<int, int> getAutomaticWinner(Team team1, Team team2);
+    std::pair<int, int> getAutomaticWinner(Team& team1, Team& team2);
 
    //updates the attributes of the teams after a game according to its outcome
-    void updateTeamsOverall(League league, Team team1, Team team2, std::pair<int, int> score);
-
+    void updateTeamsOverall(League& league, Team& team1, Team& team2, std::pair<int, int> score);
 
 };
 
