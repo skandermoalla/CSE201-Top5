dc33503f50f44aee3a6b06f48c262469a42fc2ea
SannyaBTM <45061456+SannyaBTM@users.noreply.github.com>
2019-01-10 08:11:52 +0100

    Update League.cpp

--- BTM_Qt/League.cpp
+++ BTM_Qt/League.cpp
@@ -14,7 +14,7 @@ std::vector<int> intersection(std::vector<int> vect1, std::vector<int> vect2) {
 };
 std::string teamNames[] = { "Lakers","Cavaliers","Raptors","Warriors","Thunders","76ers","Bulls","Wizards","Celtics","Pacers","Nets","Hawks" };
 
-// why a string for seas? how are you going to increment it? it should be an int.
+
 League::League(int divi, std::string seas){          //Takes arguments : division as an int and a season as a string
     division = divi ;
     season = seas;
@@ -23,12 +23,8 @@ League::League(int divi, std::string seas){          //Takes arguments : divisio
         Team t= Team( teamNames[(n + i) % 12] );
         League::teams.push_back(t);
     }
-
-    /*std::map<std::string, double,std::list> attributes;
-     attributes["season"] = seas;
-     attributes["division"] = divi;
-     */
 }
+
 League::League(){          //Takes arguments : division as an int and a season as a string
     division = 3 ;
     season = "1718";
@@ -37,13 +33,9 @@ League::League(){          //Takes arguments : division as an int and a season a
         Team t= Team( teamNames[(n + i) % 12] );
         League::teams.push_back(t);
     }
-
-    /*std::map<std::string, double,std::list> attributes;
-     attributes["season"] = seas;
-     attributes["division"] = divi;
-     */
     Calendar=calendar();
 }
+
 
 std::map<int, std::vector< std::tuple<int,int> > > League::calendar()
 {
