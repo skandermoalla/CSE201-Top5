4cbb3b524859087dfe8fe9e09cc433de67a8f574
kevinkayyy <38563987+kevinkayyy@users.noreply.github.com>
2019-01-10 09:51:57 +0100

    Update updateTeamsOverall.cpp
    to be  added to the main project in the GameEngine

--- updateTeamsOverall.cpp
+++ updateTeamsOverall.cpp
@@ -6,11 +6,11 @@ void GameEngine::updateTeamsOverall(League& league, Team& team1, Team& team2, st
     double ov1 = team1.overallgeneral;
     double ov2 = team2.overallgeneral;
     double ovMean = (ov1 + ov2)/2;
-    double Q1 = pow(10.0,ov1/30);
-    double Q2 = pow(10.0,ov2/30);
+    double Q1 = std::pow(10.0,ov1/30);
+    double Q2 = std::pow(10.0,ov2/30);
     double Exp = Q1/(Q1+Q2); //expectance for team1, between 0 and 1
 
-    double Kfactor(int overall){
+    double Kfactor(double overall){
         return 0.3*(100 - overall);
     }                      //K factor (to be improved), teams with higher scores gets smaller modifications
        
@@ -31,9 +31,18 @@ void GameEngine::updateTeamsOverall(League& league, Team& team1, Team& team2, st
         ov2New = 10;
     }
     
-    void GameEngine::setOverall(Team& team, int overallNew, int overall{
-        for int i=0; i<12 ; i++){
-            team.players[i].overallgeneral += (overallNew-overall);
+    
+    if (ov1New > 100){          //set the max as 100
+        ov1New = 100;
+    }
+    if (ov2New > 100){
+        ov2New = 100;
+    }
+    
+    void GameEngine::setOverall(Team& team, double overallNew, double overall) {
+        for (int i=0; i<12 ; i++){
+            team.players[i].afterMatchUpdate(overallNew-overall);
+            team.update_overall();
     }
 
     team1.setOverall(ov1New, ov1);
