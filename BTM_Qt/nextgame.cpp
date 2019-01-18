cd89a10c52a6003a450c803a64e5e86c56341c7b
Skander Moalla <skander.moalla@polytechnique.edu>
2019-01-18 13:54:20 +0100

    construct playingManagersTeam in nextGame constructor

--- BTM_Qt/nextgame.cpp
+++ BTM_Qt/nextgame.cpp
@@ -74,6 +74,14 @@ NextGame::NextGame(GameEngine* eng, User& theuser, League& theleague, QWidget *p
     ui->def_tactic->setVisible(false);
     ui->tactics->setVisible(false);
     ui->sub->setVisible(false);
+
+    //get user's and opponents teams
+    Team& initManagersTeam = myuser->team;
+    Team& initOpponentsTeam = myleague->getThisWeeksOpponentTeam();
+
+    //copy managers team to be able to apply changes to it and recover the initTeam for default tactic
+    playingManagersTeam = engine->copyTeam(initManagersTeam);
+    playingOpponentsTeam = engine->copyTeam(initOpponentsTeam);
 }
 
 
@@ -284,14 +292,6 @@ void NextGame::on_start_clicked()
     ui->start->setEnabled(false);
     ui->start->setVisible(false);
     ui->start->setEnabled(false);
-
-    //get user's and opponents teams
-    Team& initManagersTeam = myuser->team;
-    Team& initOpponentsTeam = myleague->getThisWeeksOpponentTeam();
-
-    //copy managers team to be able to apply changes to it and recover the initTeam for default tactic
-    playingManagersTeam = engine->copyTeam(initManagersTeam);
-    playingOpponentsTeam = engine->copyTeam(initOpponentsTeam);
 
     //show tactics button that was hidden and
     ui->start->setVisible(false);
